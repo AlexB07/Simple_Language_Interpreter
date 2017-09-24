c0b76108ec5f844d877ad6d6e980fa1beaa6abd1
Carl Hetherington <cth@carlh.net>
2017-09-24 20:59:28 +0100

    Add a welcome message.

--- src/Main.java
+++ src/Main.java
@@ -5,6 +5,7 @@ public class Main
 {
     public static void main(String[] args) {
         Interpreter interpreter = new Interpreter();
+        System.out.println("Welcome to the Excellent Versatile Interpreted Language!");
         Scanner scanner = new Scanner(System.in);
         while (true) {
             System.out.print("> ");
